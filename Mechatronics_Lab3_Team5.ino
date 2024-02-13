0d7e7d09f7290c8ac24df1aff004fac758b1e323
PeterC-Uba <84410615+PeterC-Uba@users.noreply.github.com>
2024-02-12 21:34:34 -0500

    Update Mechatronics_Lab3_Team5.ino

--- Mechatronics_Lab3_Team5.ino
+++ Mechatronics_Lab3_Team5.ino
@@ -15,7 +15,7 @@ the point turning method is suggested. You can also use your encoder signal to t
 TODO:
 1) get distance thresholds
 2) add LEDs
-3) figure out backwards and turna around
+3) figure out backwards and turn around
 
 
 */
@@ -25,31 +25,33 @@ TODO:
 
 //set pins for encoders, pwm, inputs, etc
 int M1CHA = 2; //motor 1 "inputs"
-int M1CHA = 3; //motor 1 "inputs"
+int M1CHB = 3; //motor 1 "inputs"
 
 int M2CHA = 18; //motor 2
 int M2CHB = 19; //motor 2
 
-volatile int counterM1 = 0;
-volatile int counterM2 = 0;
+volatile int encoderCountRight = 0;
+volatile int encoderCountLeft = 0;
+float countsPerDegree = (9.7 * 48) / 360.0;
+float countsFor90Degrees = countsPerDegree * 90; //may need to be calibrated/changed
 
-//sensor init
+//sensor initialization
 int signal = 52; //digital pin
 float distance;
 unsigned long pulseDuration; //USS 
 
+//LEDs for debugging:
+const int ledForward = 10;    
+const int ledBackward = 11;
+const int ledTurnLeft = 12;
+const int ledTurnRight = 13;
+const int ledStationary = 14;
 
-
-float gearRatio = 9.7;
-int CPR = 48;
-float angResolution;
-float angPosM1 = 0;
-float angle1 = 0;
-
-float angPosM2 = 0;
-float angle2 = 0;
-
-int i = 0; //this is for indexing pixy objects
+//pixy signatures
+const int SIGNATURE_FORWARD = 1;
+const int SIGNATURE_LEFT = 2;
+const int SIGNATURE_RIGHT = 3;
+const int SIGNATURE_BACKWARD = 4;
 
 
 enum Action {
@@ -71,6 +73,7 @@ void setCarState(Action newState);
 
 
 
+
 void setup() {
   // put your setup code here, to run once:
   //attach interrupt functions
@@ -78,11 +81,11 @@ void setup() {
   Serial.print("Starting...\n"); //for the pixy
   pixy.init(); //for the pixy
 
-  pinMode(ENCPINA_M1, INPUT);
-  digitalWrite(ENCPINB_M1, HIGH);
+  pinMode(M1CHA, INPUT);
+  digitalWrite(M1CHB, HIGH);
 
-  pinMode(ENCPINA_M2, INPUT);
-  digitalWrite(ENCPINB_M2, HIGH);
+  pinMode(M2CHA, INPUT);
+  digitalWrite(M2CHB, HIGH);
 
   //sensor pin
   pinMode(signal, OUTPUT);
@@ -93,12 +96,26 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(M2CHA), changeM2A, CHANGE); //interrupt channel a
   attachInterrupt(digitalPinToInterrupt(M2CHB), changeM2B, CHANGE); //interrupt channel b
 
+  pinMode(ledForward, OUTPUT);
+  pinMode(ledBackward, OUTPUT);
+  pinMode(ledTurnLeft, OUTPUT);
+  pinMode(ledTurnRight, OUTPUT);
+  pinMode(ledStationary, OUTPUT);
+
   angResolution = 360./(9.7 * 48);
+    
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+    // Turn off all LEDs initially
+  digitalWrite(ledForward, LOW);
+  digitalWrite(ledBackward, LOW);
+  digitalWrite(ledTurnLeft, LOW);
+  digitalWrite(ledTurnRight, LOW);
+  digitalWrite(ledStationary, LOW);
 
   //should we have sensors on the side in case the robot needs to reorient itself by backing up???
   motors.enableDrivers();
@@ -112,28 +129,42 @@ void loop() {
     case FORWARD:
       //both motors turn forward at the same speed (no need for feedback control because we'll use distance sensors for that)
       //motors move until certain distance is achieved from wall, turn wheel, then send back to stationary
-      RotateCW_M1();
-      RotateCW_M2();
+
+      digitalWrite(ledForward, HIGH);
 
       //if distance is within threshold, send state to stationary
       //measure distance
-      setCarState(STATIONARY);
+      if (distance >= 20){ //another trick could be while(distance>=20){rotate, reset distance}
+        RotateCW_M1();
+        RotateCW_M2();
+      }
+      else {
+        setCarState(STATIONARY);
+      }
+      
       break;
       
     case BACKWARD:
       //both motors turn backward at the same speed, until certain distance 
       //red LED turns on
-      RotateCCW_M1();
-      RotateCCW_M2();
-      setCarState(STATIONARY);
+      digitalWrite(ledBackward, HIGH);
+
+      if (distance <= 20){
+        RotateCCW_M1();
+        RotateCCW_M2();
+      }
+      else{
+        setCarState(STATIONARY);
+      }
       break;
 
     case TURN_LEFT:
       //right motor moves forward, left motor moves back
       //need someway to control how much the robot turns
       //left LED turns on
-      RotateCCW_M1();
-      RotateCW_M2();
+      digitalWrite(ledTurnLeft, HIGH);
+      
+      turnLeft();
       setCarState(STATIONARY);
       break;
 
@@ -141,8 +172,9 @@ void loop() {
       //left motor moves forward, right motor moves back
       //need someway to control how much the robot turns
       //right LED moves back
-      RotateCW_M1();
-      RotateCCW_M2();
+      digitalWrite(ledTurnRight, HIGH);
+
+      turnRight();
       setCarState(STATIONARY);
       break;
 
@@ -152,6 +184,7 @@ void loop() {
 
     case STATIONARY:
       //nothing happens
+      digitalWrite(ledStationary, HIGH);
       //if sensor sees certain color, change state to forward
       //if sensor sees another color, turn left/right/backward
       motors.setM1Speed(0);
@@ -164,20 +197,32 @@ void loop() {
       break;
 
     case PIXY_READ:
-      int detected_objects = pixy.ccc.numBlocks;
+      int detected_objects = pixy.ccc.getBlocks();
       if(detected_objects > 0){ //if greater than zero object has been detected
         for(int i = 0; i < detected_objects; i++ ){
-          if (pixy.ccc.blocks[i].m_signature == 1){ //signature of detected object 
-              setCarState(TURN_RIGHT);
-
-          }
-          if (pixy.ccc.blocks[i].m_signature == 2){
-              setCarState(TURN_LEFT);
-
-          }
-          if (pixy.ccc.blocks[i].m_signature == 3){
-              setCarState(TURN_AROUND);
-          }
+          switch (pixy.ccc.blocks[i].m_signature) {
+                case SIGNATURE_FORWARD:
+                    setCarState(FORWARD);
+                    break;
+                case SIGNATURE_LEFT:
+                    setCarState(TURN_LEFT);
+                    break;
+                case SIGNATURE_RIGHT:
+                    setCarState(TURN_RIGHT);
+                    break;
+                case SIGNATURE_BACKWARD:
+                    setCarState(BACKWARD);
+                    break;
+                // Add more cases??
+                default:
+                    // unknown signatures
+                    break;
+            }
+            // Exit the loop after setting the state based on the first recognized signature
+            break;
+        }
+        else{
+          //no objects detected
         }
       }
       break;
@@ -214,7 +259,7 @@ void RotateCW_M1(){
   {
     motors.setM1Speed(speed);
     stopIfFault();
-    delay(2);
+    //delay(2);
   }
 
 }
@@ -224,7 +269,7 @@ void RotateCW_M2(){
   {
     motors.setM2Speed(speed);
     stopIfFault();
-    delay(2);
+    //delay(2);
   }
 
 
@@ -234,9 +279,29 @@ void RotateCCW_M1(){
   {
     motors.setM1Speed(speed);
     stopIfFault();
-    delay(2);
+    //delay(2);
   }
+}
 
+  
+void turnRight(){
+    encoderCountRight = 0; // Reset right encoder count
+    encoderCountLeft = 0;  // Reset left encoder count
+    motors.setM1Speed(-200); //we want a set speed instead of a ramp up
+    motors.setM2Speed(200);
+    while(encoderCountRight < countsFor90Degrees && encoderCountLeft < countsFor90Degrees) {
+        // Keep turning until the desired encoder count is reached
+    }
+}
+void turnLeft(){
+    encoderCountRight = 0; // Reset right encoder count
+    encoderCountLeft = 0;  // Reset left encoder count
+    motors.setM1Speed(200); //we want a set speed instead of a ramp up
+    motors.setM2Speed(-200);
+    while(encoderCountRight < countsFor90Degrees && encoderCountLeft < countsFor90Degrees) {
+        // Keep turning until the desired encoder count is reached
+    }
+}
 
 }
 void RotateCCW_M2(){
@@ -244,7 +309,7 @@ void RotateCCW_M2(){
   {
     motors.setM2Speed(speed);
     stopIfFault();
-    delay(2);
+    //delay(2);
   }
 
 }
@@ -252,37 +317,37 @@ void RotateCCW_M2(){
 //encoder counts
 void changeM1A(){
   if(digitalRead(M1CHA) != digitalRead(M1CHB)){
-    counterM1++;
+    encoderCountRight++;
   }
   else{
-    counterM1--;
+    encoderCountRight--;
   }
 
 }
 void changeM1B(){
   if(digitalRead(M1CHA) != digitalRead(M1CHB)){
-    counterM1--;
+   encoderCountRight--;
   }
   else{
-    counterM1++;
+   encoderCountRight++;
   }
   
 }
 void changeM2A(){
-  if(digitalRead(M2CHA) != digitalRead(M1CHB)){
-    counterM1++;
+  if(digitalRead(M2CHA) != digitalRead(M2CHB)){
+   encoderCountLeft++;
   }
   else{
-    counterM1--;
+   encoderCountLeft--;
   }
   
 }
-void changeM1B(){
-  if(digitalRead(M2CHA) != digitalRead(M1CHB)){
-    counterM1--;
+void changeM2B(){
+  if(digitalRead(M2CHA) != digitalRead(M2CHB)){
+   encoderCountLeft--;
   }
   else{
-    counterM1++;
+    encoderCountLeft++;
   }
   
 }
